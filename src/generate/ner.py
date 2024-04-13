4cc3c26d13c66ea1388ddfafd3d9a2fafd42d67f
arunpshankar <arunprshankar@gmail.com>
2024-04-13 15:19:04 -0400

    wip

--- src/generate/ner.py
+++ src/generate/ner.py
@@ -24,7 +24,7 @@ def extract_entities(query: str) -> Dict[str, str]:
     extracted_entities['company'] = extract_entity(prompt, query)
     prompt = """Given a query, extract the specific time period from it. A valid time period should be in the form 'Q1 2021' only. 
 Examples of invalid formats include: 'Q2 2020 to Q2 2021', 'Q2 2020 - Q2 2021', 'Q2 2020, Q2 2021', and ‘Q1 2020 Q2 2020' etc. 
-The extracted time period should represent only one quarter and one year, corresponding to the present. 
+The extracted time period should represent only one quarter and one year, corresponding to the present vs past. 
 IMPORTANT: Ignore past references when the query is comparing the present to the past. 
 Translate 'first quarter of 2020' to 'Q1 2020'.
 Translate 'third quarter of fiscal year 2021' to 'Q3 2021'.
@@ -34,7 +34,18 @@ Translate 'increase in Q2 2021 compared to Q2 2020' to 'Q2 2021'.
 Translate 'change from Q2 2020 to Q2 2021' to 'Q2 2021'.
 Translate 'quarter ended Dec 31 2021' to 'Q4 2021'.
 Translate 'months ended March 31, 2022' to 'Q1 2022'.
-Translate 'months ended March 31, 2022?' to 'Q1 2022'."""
+Translate 'months ended March 31, 2022?' to 'Q1 2022'.
+Translate 'as of September 30, 2021?' to 'Q3 2021'.
+Translate 'in Q1 2022, and how does it compare to Q1 2021?' to 'Q1 2022'.
+Translate 'in Q4 2021 and Q4 2022?' to 'Q4 2022'.
+Translate 'twelve months ending December 31, 2022,' to 'Q4 2022'.
+Translate 'for Q4 2021, Q4 2022' to 'Q4 2022'.
+Translate 'six months ended December 31, 2022' to 'Q4 2022'.
+Translate 'three months ended March 31, 2023' to 'Q1 2023'.
+Translate 'increase from December 31, 2022, to June 30, 2023?' to 'Q2 2023'.
+Translate 'during Q4 2023?' to 'Q4 2023'.
+Translate 'as of Q4 2023?' to 'Q4 2023'.
+Translate 'from 2020 to the end of Q4 2023?' to 'Q4 2023'."""
     extracted_entities['time_period'] = extract_entity(prompt, query)
     logger.info(f'Query = {query}')
     logger.info(f'Extracted Entities: {extracted_entities}')
