c369c0ab29f24b2f11b5fe3cf1a9832738ad1482
Arun Shankar <arunprshankar@gmail.com>
2024-04-26 11:29:18 -0400

    wip

--- src/search/utils.py
+++ src/search/utils.py
@@ -11,6 +11,7 @@ from typing import Any
 
 LOCATION = "global" 
 
+
 def search_data_store(search_query: str, data_store_id: str) -> Optional[discoveryengine.SearchResponse]:
     """
     Searches the data store using Google Cloud's Discovery Engine API.
@@ -175,49 +176,3 @@ def create_summary_dict(matches: List[Dict[str, Any]]) -> Dict[str, Any]:
     summary_dict["match_info"] = match_info
 
     return summary_dict
-
-
-def search(query: str, data_store_id: str) -> Dict[str, Any]:
-    """
-    Searches a data store based on a given search query and brand, 
-    then consolidates the results in a dictionary.
-
-    Parameters:
-    query (str): The query used for searching the data store.
-    data_store_id (str): Vertex AI Search Data Store ID.
-
-    Returns:
-        Dict[str, Any]: A dictionary containing the consolidated results of the search.
-                        Returns an empty dictionary if an error occurs.
-    """
-    try:
-        # Perform the search with the provided query and filter
-        hits = search_data_store(query, data_store_id)
-
-        # Extract relevant data from the search results
-        matches = extract_relevant_data(hits)
-
-        # Create a summary dictionary from the matches
-        summary_dict = create_summary_dict(matches)
-
-        return summary_dict
-
-    except Exception as e:
-        # Log the error and return an empty dictionary or an error message
-        logger.error(f"Error executing search_data_store: {e}")
-        return {}
-    
-
-if __name__ == "__main__":
-    query = "What was the operating income or loss (in billions) for Google Cloud for Q1 of 2021 compared to the previous year?"
-    data_store_id = "quarterly-reports"
-
-    results = search(query, data_store_id)
-    print(results['match_info'][0].keys())
-    summarized_ans = results['summarized_answer']
-    print(f'Summarized Answer = {summarized_ans}')
-    print()
-    match_info = results['match_info']
-    for info in match_info:
-        print(info['extractive_answers'])
-        print('-' * 100)
