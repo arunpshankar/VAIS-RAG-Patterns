b7a9ff6cf9c131d43c1297136c0e085c34d1e6aa
Arun Shankar <arunprshankar@gmail.com>
2024-04-28 16:33:11 -0400

    wip

--- src/eval/retrieval/doc_search_with_filters.py
+++ src/eval/retrieval/doc_search_with_filters.py
@@ -1,28 +1,12 @@
 from src.search.doc_search_with_filters import search
 from src.generate.ner import extract_entities
 from src.config.logging import logger
+from src.eval.utils import load_data
 from typing import Tuple
 from typing import List
 from tqdm import tqdm
 import pandas as pd
 import re
-
-
-def load_data(file_path: str) -> pd.DataFrame:
-    """
-    Load CSV data from a specified file path.
-
-    Parameters:
-    file_path (str): The path to the CSV file.
-
-    Returns:
-    pd.DataFrame: A DataFrame containing the loaded data.
-    """
-    try:
-        return pd.read_csv(file_path)
-    except Exception as e:
-        logger.error(f"Failed to load data from {file_path}: {e}")
-        raise
 
 
 def validate_company(company):
